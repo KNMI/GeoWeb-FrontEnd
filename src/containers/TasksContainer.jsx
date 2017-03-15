9e6e2df10e127f25659c368fd85d40d94f1c7f84
diMosella <wim.van.moosel@gmail.com>
2017-03-15 07:28:07 +0100

    First steps in filtering tasks

--- src/containers/TasksContainer.jsx
+++ src/containers/TasksContainer.jsx
@@ -4,65 +4,71 @@ import Icon from 'react-fa';
 import CollapseOmni from '../components/CollapseOmni';
 import Panel from '../components/Panel';
 
+const items = [
+  {
+    title: 'Checklist shift',
+    notifications: 3,
+    tasks: [
+      { title: 'Basis forecast' },
+      { title: 'Guidance Model interpretation' },
+      { title: 'General Transfer' },
+      { title: 'Safety Shift Transfer' },
+      { title: 'Shift Report' }
+    ]
+  },
+  {
+    title: 'Products',
+    tasks: [
+      { title: 'Today\'s all shift products' },
+      { title: 'Shared products' },
+      { title: 'Warnings' },
+      {
+        title: 'Create SIGMET',
+        notifications: 4,
+        action: this.createSIGMET
+      },
+      { title: 'Forecasts' },
+      { title: 'Analyses' }
+    ]
+  },
+  {
+    title: 'Reports & Logs',
+    tasks: [
+      { title: 'Shift report' }
+    ]
+  },
+  {
+    title: 'Monitoring & Triggers',
+    tasks: [
+      { title: 'Extremes' }
+    ]
+  }
+];
+
 class TasksContainer extends Component {
   constructor (props) {
     super(props);
     this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
+    this.filter = this.filter.bind(this);
+    this.state = { collapse: false, filter: undefined };
   }
 
   toggle () {
-    this.setState({ collapse: !this.state.collapse });
+    this.setState({ collapse: !this.state.collapse, filter: this.state.filter });
+  }
+  filter () {
+    const filter = new RegExp(document.querySelector('.filter').value, 'i');
+    console.log('filter', document.querySelector('.filter').value);
+    this.setState({ collapse: this.state.collapse, filter: filter });
   }
 
   render () {
-    const items = [
-      {
-        title: 'Checklist shift',
-        notifications: 3,
-        tasks: [
-          { title: 'Basis forecast' },
-          { title: 'Guidance Model interpretation' },
-          { title: 'General Transfer' },
-          { title: 'Safety Shift Transfer' },
-          { title: 'Shift Report' }
-        ]
-      },
-      {
-        title: 'Products',
-        tasks: [
-          { title: 'Today\'s all shift products' },
-          { title: 'Shared products' },
-          { title: 'Warnings' },
-          {
-            title: 'Create SIGMET',
-            notifications: 4,
-            action: this.createSIGMET
-          },
-          { title: 'Forecasts' },
-          { title: 'Analyses' }
-        ]
-      },
-      {
-        title: 'Reports & Logs',
-        tasks: [
-          { title: 'Shift report' }
-        ]
-      },
-      {
-        title: 'Monitoring & Triggers',
-        tasks: [
-          { title: 'Extremes' }
-        ]
-      }
-    ];
-
     let title = <Form inline>
       <Button color='primary' onClick={this.toggle}>{this.state.collapse ? '«' : '»'}</Button>
       <InputGroup>
-        <Input placeholder='search term&hellip;' />
+        <Input className='filter' placeholder='search term&hellip;' />
         <InputGroupButton>
-          <Button className='btn-outline-info'>Search</Button>
+          <Button className='btn-outline-info' onClick={this.filter}>Search</Button>
         </InputGroupButton>
       </InputGroup>
     </Form>;
@@ -70,7 +76,8 @@ class TasksContainer extends Component {
       <Col className='TasksContainer'>
         <CollapseOmni className='CollapseOmni' isOpen={this.state.collapse} isHorizontal minSize={64} maxSize={300}>
           <Panel className='Panel' title={title}>
-            {items.map((item, index) => <TaskCategory key={index} title={item.title} notifications={item.notifications} tasks={item.tasks} />)}
+            {items.filter(item => this.state.filter ? this.state.filter.test(item.title) : true)
+              .map((item, index) => <TaskCategory key={index} title={item.title} notifications={item.notifications} tasks={item.tasks} />)}
           </Panel>
         </CollapseOmni>
       </Col>
