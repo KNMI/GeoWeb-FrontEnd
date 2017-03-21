ff083968379d2335d805a9acd42478fd57879ba0
Maarten Terpstra <maarten.terpstra@knmi.nl>
2017-03-21 11:47:20 +0100

    Can show trivial progtemp

--- src/routes/ADAGUC/components/Adaguc.js
+++ src/routes/ADAGUC/components/Adaguc.js
@@ -260,23 +260,11 @@ export default class Adaguc extends React.Component {
             webmapjs={this.webMapJS}
             isInEditMode={adagucProperties.mapMode === 'measure'} />
           <ModelTime webmapjs={this.webMapJS} />
-          {/* <ProgTemp webmapjs={this.webmapjs} isOpen={adagucProperties.mapMode === 'progtemp'} /> */}
         </div>
       </div>
     );
   }
 };
-
-// class ProgTemp extends React.Component {
-//   render () {
-//     const { webmapjs, isOpen } = this.props;
-//     if (isOpen) {
-//       return <Popout url='progtemp'><span>Hi</span></Popout>;
-//     } else {
-//       return <div />;
-//     }
-//   }
-// }
 
 class ModelTime extends React.Component {
   constructor () {
