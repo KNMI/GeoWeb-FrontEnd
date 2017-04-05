3c8bbeb59793d66faae2418ee08c49499e9bc9b8
diMosellaAtWork <wim.van.moosel@knmi.nl>
2017-04-05 11:10:57 +0000

    Added unit tests

---
+++ src/components/ProductCategory.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { default as ProductCategory } from './ProductCategory';
+import { mount } from 'enzyme';
+
+describe('(Container) ProductCategory', () => {
+  it('Renders a ProductCategory', () => {
+    const _component = mount(<ProductCategory title={'test'} icon='star' />);
+    expect(_component.type()).to.eql(ProductCategory);
+  });
+});
