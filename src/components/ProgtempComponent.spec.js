c6154f0f1e0817e93bcc48b72bc1f2a109cf714c
Maarten Terpstra <maarten.terpstra@knmi.nl>
2017-04-05 14:40:05 +0200

    Panels are cool now and more tests

---
+++ src/components/ProgtempComponent.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { default as ProgtempComponent } from './ProgtempComponent';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+const adagucProperties = {
+  wmjslayers: {
+    layers: [],
+    baselayers: [],
+    overlays: []
+  }
+};
+
+const emptyDispatch = () => {};
+const emptyActions = {};
+
+describe('(Component) ProgtempComponent', () => {
+  it('Renders a ProgtempComponent', () => {
+    global.drawProgtempBg = sinon.stub().returns('asdf');
+    const _component = mount(<ProgtempComponent adagucProperties={adagucProperties} dispatch={emptyDispatch} actions={emptyActions} />);
+    expect(_component.type()).to.eql(ProgtempComponent);
+  });
+});
