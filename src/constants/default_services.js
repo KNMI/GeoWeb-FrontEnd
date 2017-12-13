4790e40c2cb4ff5a0485f490cf452624be729ec0
maartenplieger <maarten.plieger@knmi.nl>
2017-12-13 17:11:31 +0100

    Fixed URL endpoints for progtemp diagram

--- src/constants/default_services.js
+++ src/constants/default_services.js
@@ -1,9 +1,9 @@
 import { WEBSERVER_URL } from '../static/urls.json';
-export const MODEL_LEVEL_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=HARM_N25_ML&`;
-export const HARMONIE_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=HARM_N25&`;
-export const HARMONIE_ML_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=HARM_N25_ML&`;
-export const OVERLAY_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=OVL&`;
-export const OBSERVATIONS_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=OBS&`;
-export const RADAR_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=RADAR&`;
+export const MODEL_LEVEL_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=HARM_N25_ML&`;
+export const HARMONIE_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=HARM_N25&`;
+export const HARMONIE_ML_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=HARM_N25_ML&`;
+export const OVERLAY_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=OVL&`;
+export const OBSERVATIONS_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=OBS&`;
+export const RADAR_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=RADAR&`;
 export const LIGHTNING_URL = 'http://bvmlab-218-41.knmi.nl/cgi-bin/WWWRADAR3.cgi?';
-export const SATELLITE_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adaguc-server?DATASET=SAT&`;
+export const SATELLITE_URL = `${WEBSERVER_URL}/ogc/adaguc-services/adagucserver?DATASET=SAT&`;
