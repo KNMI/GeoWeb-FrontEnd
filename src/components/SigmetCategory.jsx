3cad0318668d15d6b6f4560ce041ad7ce9f7ebc6
diMosellaAtWork <wim.van.moosel@knmi.nl>
2018-02-12 11:59:33 +0100

    GEOW-518

--- src/components/SigmetCategory.jsx
+++ src/components/SigmetCategory.jsx
@@ -953,15 +953,13 @@ class SigmetCategory extends Component {
   render () {
     const { title, icon, parentCollapsed, editable, selectedIndex, toggleMethod, drawProperties } = this.props;
     const notifications = !editable ? this.state.list.length : 0;
-    console.log('Features', this.state.list, drawProperties);
     // Show a warning in case there is no drawing yet, so both the this.state.list and the this.props.drawProperties are empty
-    const showDrawWarning = !this.state.list.length > 0 || !this.state.list[0].hasOwnProperty('geojson') || !this.state.list[0].geojson.hasOwnProperty('features') ||
+    const showDrawWarningFromState = !this.state.list.length > 0 || !this.state.list[0].hasOwnProperty('geojson') || !this.state.list[0].geojson.hasOwnProperty('features') ||
       !this.state.list[0].geojson.features.length > 0 || !this.state.list[0].geojson.features[0].hasOwnProperty('geometry') ||
-      !this.state.list[0].geojson.features[0].geometry.hasOwnProperty('coordinates') || !this.state.list[0].geojson.features[0].geometry.coordinates.length > 0 ||
-      !drawProperties || !drawProperties.hasOwnProperty('geojson') || !drawProperties.geojson.hasOwnProperty('features') ||
+      !this.state.list[0].geojson.features[0].geometry.hasOwnProperty('coordinates') || !this.state.list[0].geojson.features[0].geometry.coordinates.length > 0;
+    const showDrawWarningFromProps = !drawProperties || !drawProperties.hasOwnProperty('geojson') || !drawProperties.geojson.hasOwnProperty('features') ||
       !drawProperties.geojson.features.length > 0 || !drawProperties.geojson.features[0].hasOwnProperty('geometry') ||
       !drawProperties.geojson.features[0].geometry.hasOwnProperty('coordinates') || !drawProperties.geojson.features[0].geometry.coordinates.length > 0;
-    console.log('Show', showDrawWarning);
     let maxSize = this.state.list ? 500 * this.state.list.length : 0;
     if (editable) {
       maxSize = 900;
@@ -1133,7 +1131,7 @@ class SigmetCategory extends Component {
                         </Col>
                       </Row>
                     }
-                    {selectedIndex && editable && showDrawWarning
+                    {selectedIndex > -1 && editable && (showDrawWarningFromState && showDrawWarningFromProps)
                       ? <Row style={{ flex: 'none', padding: '0.5rem 0 0.5rem 0.12rem', maxWidth: '22.5rem' }}>
                         <Col>
                           <Alert color='danger' style={{ display: 'block', margin: '0', whiteSpace: 'normal', padding: '0.75rem' }}>
@@ -1147,7 +1145,7 @@ class SigmetCategory extends Component {
                     {editable
                       ? <Row style={{ minHeight: '2.5rem' }}>
                         <Col xs={{ size: 3, offset: 9 }}>
-                          <Button color='primary' onClick={this.saveSigmet} >Save</Button>
+                          <Button color='primary' disabled={selectedIndex === -1} onClick={this.saveSigmet} >Save</Button>
                         </Col>
                       </Row>
                       : ''
