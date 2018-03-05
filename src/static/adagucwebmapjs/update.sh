8c1eaf1beb8e62ccf5e5486e1f17fb0c4bcec0c1
maartenplieger <maarten.plieger@knmi.nl>
2018-03-05 11:17:33 +0100

    Together with maartenlterpstra worked on subsetting time based on reference_time

---
+++ src/static/adagucwebmapjs/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget "https://raw.githubusercontent.com/KNMI/adaguc-viewer/master/builds/adagucwebmapjs/webmapjs.min.css" -O ./webmapjs.min.css
+wget "https://raw.githubusercontent.com/KNMI/adaguc-viewer/master/builds/adagucwebmapjs/webmapjs.min.js" -O ./webmapjs.min.js
