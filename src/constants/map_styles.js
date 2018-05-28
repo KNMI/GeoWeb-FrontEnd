79b2509911be6bc71f36b73cf01d4aeee1a96aa3
Maarten Terpstra <maarten.terpstra@knmi.nl>
2018-05-28 16:53:42 +0200

    Added s3 ingmar map

--- src/constants/map_styles.js
+++ src/constants/map_styles.js
@@ -51,11 +51,43 @@ const KNMI_NATURALEARTH2 = {
 const MWS_STYLE = {
   service: 'http://geoservices.knmi.nl/cgi-bin/worldmaps.cgi?',
   name: 'mwsmap',
-  transparent: false,
+  transparent: true,
   title: 'KNMI MWS style map',
   format: 'image/png',
   enabled: true,
   dimensions: []
 };
 
-export const MAP_STYLES = [OSM_STYLE, KNMI_NATURALEARTH2, MWS_STYLE, ESRI_ARCGIS_CANVAS, ESRI_ARCGIS_TOPO, ESRI_ARCGIS_SAT, ESRI_ARCGIS_OCEAN];
+const INGMAPPING_WORLDMAP = {
+  title:'WorldMap',
+  name:'WorldMap',
+  type: 'twms',
+  enabled:true
+};
+const INGMAPPING_WORLDMAP_LIGHT = {
+  title:'WorldMap_Light',
+  name:'WorldMap_Light',
+  type: 'twms',
+  enabled:true
+};
+const INGMAPPING_WORLDMAP_CANVAS = {
+  title:'WorldMap_Canvas',
+  name:'WorldMap_Canvas',
+  type: 'twms',
+  enabled:true
+};
+const INGMAPPING_WORLDMAP_GREYCANVAS = {
+  title:'WorldMap_GreyCanvas',
+  name:'WorldMap_GreyCanvas',
+  type: 'twms',
+  enabled:true
+};
+const INGMAPPING_WORLDMAP_LIGHT_GRAY_CANVAS = {
+  title:'WorldMap_LightGreyCanvas',
+  name:'WorldMap_LightGreyCanvas',
+  type: 'twms',
+  enabled:true
+};
+
+export const MAP_STYLES = [OSM_STYLE, KNMI_NATURALEARTH2, MWS_STYLE, ESRI_ARCGIS_CANVAS, ESRI_ARCGIS_TOPO, ESRI_ARCGIS_SAT, ESRI_ARCGIS_OCEAN, INGMAPPING_WORLDMAP,
+  INGMAPPING_WORLDMAP_LIGHT, INGMAPPING_WORLDMAP_CANVAS, INGMAPPING_WORLDMAP_GREYCANVAS, INGMAPPING_WORLDMAP_LIGHT_GRAY_CANVAS];
