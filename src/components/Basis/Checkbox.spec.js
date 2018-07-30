19dab81e1a366fe41ef6dd6724156a42c35bc942
diMosellaAtWork <wim.van.moosel@knmi.nl>
2018-07-30 17:21:30 +0200

    Added unit tests for basis components

---
+++ src/components/Basis/Checkbox.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+import { mount } from 'enzyme';
+
+describe('(Component) Basis/Checkbox', () => {
+  it('renders a Checkbox', () => {
+    const _component = mount(<Checkbox option={{ optionId: 0, label: 'checked' }} />);
+    expect(_component.type()).to.eql(Checkbox);
+  });
+});
