d826acf565a3bdb9e0ff53438d6d3705e23bce15
rturtolo <rocco.turtolo@knmi.nl>
2019-07-01 18:39:05 +0200

    fix(config): added host and port as environment variables, fixed a typo.

--- config/project.config.js
+++ config/project.config.js
@@ -25,7 +25,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : 'localhost', // ip.address(), // use string 'localhost' to prevent exposure on local network
+  server_host : process.env.HOST || 'localhost', // ip.address(), // use string 'localhost' to prevent exposure on local network
   server_port : process.env.PORT || 3000,
 
   // ----------------------------------
