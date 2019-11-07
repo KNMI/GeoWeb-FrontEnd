5e897e4f7aeae72b96b2592f270ff60499d9a62b
Maarten Plieger <maartenplieger@gmail.com>
2019-11-07 09:41:43 +0000

    New TAF selector and no race conditions for buttons

--- src/components/Taf/TafSelector.jsx
+++ src/components/Taf/TafSelector.jsx
@@ -1,16 +1,18 @@
 import React, { PureComponent } from 'react';
-import { Row, Col, Alert } from 'reactstrap';
-import { Typeahead } from 'react-bootstrap-typeahead';
+import { Row, Col, Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Icon } from 'react-fa';
-import { TAF_TYPES } from './TafTemplates';
-import { MODALS } from '../../containers/Taf/TafActions';
+import { TAF_TYPES, TIMELABEL_FORMAT } from './TafTemplates';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 export default class TafSelector extends PureComponent {
   constructor (props) {
     super(props);
     this.equalsLocationOrTime = this.equalsLocationOrTime.bind(this);
-    this.triggerModal = this.triggerModal.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
   }
 
   equalsLocationOrTime (option, props) {
@@ -19,49 +21,72 @@ export default class TafSelector extends PureComponent {
       option.label.status.toLowerCase().indexOf(props.text.toLowerCase()) !== -1;
   }
 
-  triggerModal (evt) {
-    const { onChange, dispatch, actions, selectedTaf } = this.props;
-    evt.preventDefault();
-    evt.stopPropagation();
-    if (!Array.isArray(selectedTaf) || selectedTaf.length === 0) {
-      return;
-    }
-    if (!selectedTaf[0].hasEdits) {
-      onChange([]);
-      return;
-    }
-    dispatch(actions.toggleTafModalAction(evt, MODALS.CONFIRM_SWITCH.type));
+  toggle () {
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
   }
 
+  renderTaf (taf) {
+    let baseTime = taf.label.time;
+    if (taf && taf.tafData && taf.tafData.metadata && taf.tafData.metadata.baseTime) {
+      const formattedBaseTime = moment.utc(taf.tafData.metadata.baseTime).format(TIMELABEL_FORMAT);
+      const formattedValidityStart = moment.utc(taf.tafData.metadata.validityStart).format(TIMELABEL_FORMAT);
+      if (formattedBaseTime !== formattedValidityStart) {
+        baseTime = formattedBaseTime + ' -> ' + formattedValidityStart;
+      }
+    }
+    return (<Row style={{ display: 'inline-block', cursor: 'pointer', width: '100%' }}>
+      <Col xs='1'><Icon name={taf.label.icon} /></Col>
+      <Col xs='2'>{taf.location}</Col>
+      <Col xs='2'>{baseTime}</Col>
+      <Col xs='2'>{taf.label.date}</Col>
+      <Col xs='5'>{taf.label.status}</Col>
+    </Row>);
+  }
+  renderTafs (tafs, selectTaf) {
+    /**
+     * Sort the tafs based on location and start time.
+     * @param {*} tafA
+     * @param {*} tafB
+     */
+    const tafSortingFunction = (tafA, tafB) => {
+      if (tafA.location < tafB.location) return -1;
+      if (tafA.location > tafB.location) return 1;
+      if (tafA.tafData && tafA.tafData.metadata && tafA.tafData.metadata.validityStart &&
+        tafB.tafData && tafB.tafData.metadata && tafB.tafData.metadata.validityStart) {
+        const baseTimeA = tafA.tafData.metadata.baseTime || tafA.tafData.metadata.validityStart;
+        const baseTimeB = tafB.tafData.metadata.baseTime || tafB.tafData.metadata.validityStart;
+        if (baseTimeA < baseTimeB) return -1;
+        if (baseTimeA > baseTimeB) return 1;
+      }
+      return 0;
+    };
+    return tafs.sort(tafSortingFunction).map((taf, key) => {
+      return (<DropdownItem key={key} onClick={() => { selectTaf([taf]); }} style={{ width: '100%' }}>
+        { this.renderTaf(taf) }
+      </DropdownItem>);
+    });
+  }
   render () {
     const { selectableTafs, selectedTaf, onChange } = this.props;
     return <Row className='TafSelector'>
-      <Col xs='auto'>TAF for</Col>
-      <Col xs='3'>
-        <Typeahead
-          filterBy={this.equalsLocationOrTime}
-          labelKey={option => `${option.label.text} ${option.label.status}`}
-          options={selectableTafs} onChange={onChange} onFocus={this.triggerModal}
-          selected={selectedTaf || []} placeholder={'Select a TAF'}
-          renderMenuItemChildren={(option, props, index) => {
-            return <Row>
-              <Col xs='1'><Icon name={option.label.icon} /></Col>
-              <Col xs='2'>{option.location}</Col>
-              <Col xs='2'>{option.label.time}</Col>
-              <Col xs='2'>{option.label.date}</Col>
-              <Col xs='5'>{option.label.status}</Col>
-            </Row>;
-          }}
-          clearButton />
+      <Col xs='7'>
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{ width: '800px' }}>
+          <DropdownToggle caret style={{ width: '100%', color: 'black' }}>
+            { selectedTaf && selectedTaf.length === 1 ? this.renderTaf(selectedTaf[0]) : 'Select a TAF' }
+          </DropdownToggle>
+          <DropdownMenu style={{ width: '800px', paddingLeft: '10px' }}>
+            <Button style={{ float: 'right' }} color='link' onClick={() => { const { dispatch, actions } = this.props; dispatch(actions.updateTafsAction()); }}>
+              <abbr style={{ cursor: 'pointer' }} title='Refresh the list of TAFs'><Icon name='refresh' /></abbr>
+            </Button>
+
+            <DropdownItem header>Published TAF's</DropdownItem>
+            { this.renderTafs(selectableTafs.filter(taf => taf.tafData.metadata.status === 'published'), onChange) }
+            <DropdownItem divider />
+            <DropdownItem header>TAF's todo</DropdownItem>
+            { this.renderTafs(selectableTafs.filter(taf => taf.tafData.metadata.status !== 'published'), onChange) }
+          </DropdownMenu>
+        </Dropdown>
       </Col>
-      {!selectedTaf || !Array.isArray(selectedTaf) || selectedTaf.length === 0
-        ? <Col xs='4'>
-          <Alert color={'warning'}>
-            <Icon name='exclamation-triangle' /> Currently, no TAF is selected.
-          </Alert>
-        </Col>
-        : null
-      }
     </Row>;
   }
 }
